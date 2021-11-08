892fcf4359e9f3043ad9f0b573a9a19bb89bcc9f
Rafael Correa <scriptsengineer@gmail.com>
2021-11-07 21:17:01 -0300

    Update new package settings

--- Runtime/Scripts/Providers/TransformRayProvider.cs
+++ Runtime/Scripts/Providers/TransformRayProvider.cs
@@ -8,13 +8,9 @@ namespace ExpressoBits.Interactions
         [SerializeField] private Transform cameraTransform;
         [SerializeField] private float distance;
 
-        private void Awake()
-        {
-            cameraTransform = Camera.main.transform;
-        }
-
         public Ray CreateRay()
         {
+            if(cameraTransform == null) cameraTransform = Camera.main.transform;
             distance = Vector3.Distance(cameraTransform.position,transform.position);
             return new Ray(cameraTransform.position + cameraTransform.forward * distance/2f, cameraTransform.forward);
         }
